2e2aa249942e54751909960af49a57153428504f
dao1oad <86087622+dao1oad@users.noreply.github.com>
2026-03-08 20:37:05 +0800

    style: format sell price type compatibility test

--- freshquant/tests/test_sell_price_type_compat.py
+++ freshquant/tests/test_sell_price_type_compat.py
@@ -4,9 +4,10 @@ from freshquant.order_management.submit.execution_bridge import (
 
 
 def test_resolve_sell_price_type_compat_prefers_broker_price_type():
-    assert resolve_sell_price_type_compat(
-        {"broker_price_type": 13, "price_type": 99}
-    ) == 13
+    assert (
+        resolve_sell_price_type_compat({"broker_price_type": 13, "price_type": 99})
+        == 13
+    )
 
 
 def test_resolve_sell_price_type_compat_falls_back_to_legacy_price_type():
